240f450313b2fd6a460e08d537ce0234237a9db5
Stefano Modenese <62080596+stefanoModenese@users.noreply.github.com>
2024-08-25 15:42:37 +0200

    Time: 37 ms (68.90%) | Memory: 11.7 MB (30.53%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -4,16 +4,8 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        num = list(str(x))
-        i = 0
-        j = len(num) - 1
-
-        while (i < j):
-            if (num[i] == num[j]):
-                i = i + 1
-                j = j - 1
-            else:
-                return False
-        
-        return True
-        
+        x = str(x)
+        if (x == x[::-1]):
+            return True
+        else:
+             return False        
