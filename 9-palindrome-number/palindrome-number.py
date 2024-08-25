7197743826de9380b2ddf58a5c6f22af9c7e9567
Stefano Modenese <62080596+stefanoModenese@users.noreply.github.com>
2024-08-25 15:34:18 +0200

    Time: 40 ms (57.94%) | Memory: 11.7 MB (30.53%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        num = list(str(x))
+        i = 0
+        j = len(num) - 1
+
+        while (i != j and i < j):
+            if (num[i] == num[j]):
+                i = i + 1
+                j = j - 1
+            else:
+                return False
+        
+        return True
+        
